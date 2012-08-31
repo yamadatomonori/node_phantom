7325976d89b71ce85103dee7bfe3c3ed0a28c73d
yamadatomonori <tomonori.yamada.g+heroku@gmail.com>
2012-08-31 18:22:03 +0900

    add testSuites.js

--- test/phantom.js
+++ test/phantom.js
@@ -8,16 +8,11 @@ page.onConsoleMessage = function(msg) {
   console.log(msg);
 };
 
+page.injectJs('testSuites.js');
 
-page.open('http://quiet-castle-1767.herokuapp.com/test/index.html', function() {
-  /*
-  var firstChild = page.evaluate(function() {
-    return document.body.firstChild:
-  });
+page.open(
+    'http://quiet-castle-1767.herokuapp.com/test/index.html', function() {
 
-  console.log(firstChild);
-  */
-
-  phantom.exit();
-});
+      phantom.exit();
+    });
 
