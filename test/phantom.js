da36d52c08449e2c083e1db72b8da6c3cf6a8d03
yamadatomonori <tomonori.yamada.g+heroku@gmail.com>
2012-08-31 17:19:44 +0900

    modify phantom.js

--- test/phantom.js
+++ test/phantom.js
@@ -8,7 +8,10 @@ page.onConsoleMessage = function(msg) {
   console.log(msg);
 };
 
-page.open('http://quiet-castle-1767.herokuapp.com/test/index.html');
+page.open('http://quiet-castle-1767.herokuapp.com/test/index.html', function() {
+  console.log(222);
+});
+
 console.log(111);
 
 phantom.exit();
